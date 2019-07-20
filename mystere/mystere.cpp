d35a5af8237420e1043b7e31a4179f3ced56ad42
paindespik <seandriard3@gmail.com>
2019-07-20 15:42:34 +0200

    new commit

---
+++ mystere/mystere.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <limits>  
+using namespace std;
+
+int main(){
+	cout << "quel est votre mot mystère?: " <<endl;
+	char mystere[1024];
+	
+
+	cin >> mystere;
+	
+	cout <<"beuleu"<<endl;
+	//tab.push_back(mystere.c_str());
+	cout <<mystere<< endl; 
+
+}
