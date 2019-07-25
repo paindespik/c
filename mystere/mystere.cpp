ce63b28808b47b99a283231b2e7486db7568c8d3
paindespik <seandriard3@gmail.com>
2019-07-25 13:49:54 +0200

    new commit

--- mystere/mystere.cpp
+++ mystere/mystere.cpp
@@ -3,6 +3,9 @@
 #include <cstdlib>
 #include <vector>
 #include <fstream>
+#include <ctype.h>
+#include <algorithm> 
+#include <string>  
 using namespace std;
 int main() {
 	ifstream monFlux("/home/driard/c++/mystere/mot.txt");
@@ -10,27 +13,36 @@ int main() {
 	string essai;
 	string mot;
 	string melange;
+	int nb_cout(4);
+	char recommencer='o';
 	if (monFlux){
 		while (getline(monFlux,mot)){
 			tab_mot.push_back(mot);
 		}
 	}
-	
-	srand(time(0));
-	mot=tab_mot[rand()%tab_mot.size()];
+
+	while(recommencer=='o'){
+			srand(time(0));
+		mot=tab_mot[rand()%tab_mot.size()];
 
 
 
-	melange=melanger(mot);
-	cout << "le mot mélangé est " << melange << endl;
-	cout << "quel est le mot mystère?:  " << endl;
-	cin >> essai;
-	while (essai!=mot){
-		cout <<"bad" <<endl;
+		melange=melanger(mot);
+		cout << "le mot mélangé est " << melange << endl;
 		cout << "quel est le mot mystère?:  " << endl;
 		cin >> essai;
-	}
-	cout << "good" << endl;
+		transform(essai.begin(), essai.end(), essai.begin(), ::toupper);
+		while (essai!=mot && nb_cout>0){
+			cout <<"bad" <<endl;
+			cout << "quel est le mot mystère?: (il vous reste "<< nb_cout <<") " << endl;
+			cin >> essai;
+			nb_cout--;
+		}
+		cout << "good" << endl;
+		cout << "voulez-vous recommencer? : (o/n) "<<endl;
+		cin >> recommencer;
+		}
+
 
 
 
